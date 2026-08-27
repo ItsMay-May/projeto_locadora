3561b16448cc10dde43007b26cbfdce2198386e0
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-08-27 14:04:07 -0300

    atualização dos exercicios

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
 from jogos import (salvar_jogos)
 from locacoes import (registro_locacoes)
 from clientes import( cadastro_clientes)
+from persistencia import(
+    carregar_clientes,
+    carregar_jogos,
+    carregar_locacoes
+)
+
+def menu():
+    while(True):
+        print("---Bem vindo a locadora do Tiago Postgres, escolha uma das opções abaixo:---")
+        print("\n1-Cadastrar jogos \n2-Cadastrar novo cliente \n-Registrar locação \n4-Listar jogos disponíveis \n5- Ver clientes cadastrados \n6-Ver resistros de locações \n7- Sair")
+        cod = input("Insira aqui: ")
+
+        if cod == 1:
+            salvar_jogos()
+        elif cod == 2:
+            cadastro_clientes()
+        elif cod == 3:
+            registro_locacoes()
+        elif cod == 4:
+            carregar_jogos()
+        elif cod == 5:
+            carregar_clientes()
+        elif cod == 6:
+            carregar_locacoes()
+        elif cod == 7:
+            print("Saindo ...")
+            break
